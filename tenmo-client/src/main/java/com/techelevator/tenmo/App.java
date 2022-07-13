d46f4433886115b28aca0ba5e7fde7b7db5b5be3
Seth Post <postsa@miamioh.edu>
2022-07-13 12:13:58 -0400

    Added print out messages for main menu 3 and 5, various tweaks

--- tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 public class App {
 
     private static final String API_BASE_URL = "http://localhost:8080/";
+    private final BigDecimal ZERO = new BigDecimal("0.00");
 
     private final ConsoleService consoleService = new ConsoleService();
     private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
@@ -82,7 +83,7 @@ public class App {
             } else if (menuSelection == 5) {
                 requestBucks();
             } else if (menuSelection == 0) {
-                continue;
+                thankYou();
             } else {
                 System.out.println("Invalid Selection");
             }
@@ -102,7 +103,8 @@ public class App {
 
 	private void viewTransferHistory() {
 		// TODO Auto-generated method stub
-        int userId = consoleService.promptForInt("Please enter your user ID number: ");
+        long longUserId = currentUser.getUser().getId();
+        int userId = (int)longUserId;
         Transfer[] transfers = userService.listTransfers(userId);
         User toUser = null;
         User fromUser = null;
@@ -120,27 +122,48 @@ public class App {
             System.out.println(transfer.toString());
         }
         System.out.println();
-        int transferId = consoleService.promptForInt("Please enter a transfer Id number to view details: ");
-        Transfer specificTransfer = userService.getTransferById(transferId);
-        if (specificTransfer != null) {
-            System.out.println("---------------------");
-            System.out.println("Transfer Details");
-            System.out.println("---------------------");
-            System.out.println("Id: " + specificTransfer.getId());
-            System.out.println("From: " + fromUser.getUsername());
-            System.out.println("To: " + toUser.getUsername());
-            System.out.println("Type: " + specificTransfer.getTypeDescription());
-            System.out.println("Status: " + specificTransfer.getStatusDescription());
-            System.out.println("Amount: " + specificTransfer.getAmount());
+        int transferId = consoleService.promptForInt("Please enter a transfer Id number to view details, or press '0' to exit: ");
+        if (transferId == 0) {
         } else {
-            consoleService.printErrorMessage();
+            Transfer specificTransfer = null;
+            while (specificTransfer == null) {
+                specificTransfer = userService.getTransferById(transferId);
+                if (specificTransfer != null) {
+                    System.out.println("---------------------");
+                    System.out.println("Transfer Details");
+                    System.out.println("---------------------");
+                    System.out.println("Id: " + specificTransfer.getId());
+                    System.out.println("From: " + fromUser.getUsername());
+                    System.out.println("To: " + toUser.getUsername());
+                    System.out.println("Type: " + specificTransfer.getTypeDescription());
+                    System.out.println("Status: " + specificTransfer.getStatusDescription());
+                    System.out.println("Amount: " + specificTransfer.getAmount());
+                } else {
+                    transferId = consoleService.promptForInt("Please enter a valid transfer Id number, or press '0' to exit: ");
+                    if (transferId == 0) {
+                        break;
+                    } else {
+                        specificTransfer = userService.getTransferById(transferId);
+                        if (specificTransfer != null) {
+                            System.out.println("---------------------");
+                            System.out.println("Transfer Details");
+                            System.out.println("---------------------");
+                            System.out.println("Id: " + specificTransfer.getId());
+                            System.out.println("From: " + fromUser.getUsername());
+                            System.out.println("To: " + toUser.getUsername());
+                            System.out.println("Type: " + specificTransfer.getTypeDescription());
+                            System.out.println("Status: " + specificTransfer.getStatusDescription());
+                            System.out.println("Amount: " + specificTransfer.getAmount());
+                        }
+                    }
+                }
+            }
         }
-		
 	}
 
 	private void viewPendingRequests() {
 		// TODO Auto-generated method stub
-		
+        System.out.println("We're sorry, that service is currently unavailable.");
 	}
 
 	private void sendBucks() {
@@ -162,33 +185,85 @@ public class App {
         String fromUserName = fromUser.getUsername();
         transferEnteredByUser.setFromUserName(fromUserName);
 
-        int toUserId = consoleService.promptForInt("Please enter the user ID for the person you would like to send TE bucks to: ");
-        User toUser = userService.getUserById(toUserId);
-        Account toAccount = userService.getAccountByUserId(toUserId);
-        int toAccountId = toAccount.getId();
-        transferEnteredByUser.setAccountToId(toAccountId);
+        int toUserId = consoleService.promptForInt("Please enter the user ID for the person you would like to send TE bucks to, or press '0' to exit: ");
+        if (toUserId == 0) {
 
-        String toUserName = toUser.getUsername();
-        transferEnteredByUser.setToUserName(toUserName);
-
-        BigDecimal amount = consoleService.promptForBigDecimal("Please enter the amount of TE bucks you would like to send: ");
-        if (amount.compareTo(new BigDecimal("0.00")) <= 0) {
-            consoleService.printErrorMessage();
-        } else {
-            transferEnteredByUser.setAmount(amount);
         }
+        else {
+            User toUser = userService.getUserById(toUserId);
+            while (toUserId == currentUser.getUser().getId() || toUser == null) {
+                if (toUserId == currentUser.getUser().getId()) {
+                    toUserId = consoleService.promptForInt("You cannot enter your own ID. Please try again: ");
+                    toUser = userService.getUserById(toUserId);
+                } else if (toUser == null) {
+                    toUserId = consoleService.promptForInt("Please enter a valid user ID, or press '0' to exit: ");
+                    toUser = userService.getUserById(toUserId);
+                    if (toUserId == 0) {
+                        break;
+                    }
+                } else {
+                    Account toAccount = userService.getAccountByUserId(toUserId);
+                    int toAccountId = toAccount.getId();
+                    transferEnteredByUser.setAccountToId(toAccountId);
 
-        Transfer transferFromApi = userService.sendTransfer(transferEnteredByUser);
-        if (transferFromApi == null) {
-            consoleService.printErrorMessage();
-        } else {
-            System.out.println("TE bucks sent successfully!");
+                    String toUserName = toUser.getUsername();
+                    transferEnteredByUser.setToUserName(toUserName);
+
+                    BigDecimal amount = consoleService.promptForBigDecimal("Please enter the amount of TE bucks you would like to send, or press '0' to exit: ");
+                    if (amount.compareTo(ZERO) <= 0) {
+                        while (amount.compareTo(ZERO) < 0) {
+                            amount = consoleService.promptForBigDecimal("Please enter a number greater than 0, or press '0' to exit: ");
+                        }
+                        if (amount.equals(ZERO)) {
+
+                        }
+                    } else {
+                        transferEnteredByUser.setAmount(amount);
+
+                        Transfer transferFromApi = userService.sendTransfer(transferEnteredByUser);
+                        if (transferFromApi == null) {
+                            consoleService.printErrorMessage();
+                        } else {
+                            System.out.println("TE bucks sent successfully!");
+                        }
+                    }
+                }
+            }
+            Account toAccount = userService.getAccountByUserId(toUserId);
+            int toAccountId = toAccount.getId();
+            transferEnteredByUser.setAccountToId(toAccountId);
+
+            String toUserName = toUser.getUsername();
+            transferEnteredByUser.setToUserName(toUserName);
+
+            BigDecimal amount = consoleService.promptForBigDecimal("Please enter the amount of TE bucks you would like to send, or press '0' to exit: ");
+            if (amount.compareTo(ZERO) <= 0) {
+                while (amount.compareTo(ZERO) < 0) {
+                    amount = consoleService.promptForBigDecimal("Please enter a number greater than 0, or press '0' to exit: ");
+                }
+                if (amount.equals(ZERO)) {
+
+                }
+            } else {
+                transferEnteredByUser.setAmount(amount);
+
+                Transfer transferFromApi = userService.sendTransfer(transferEnteredByUser);
+                if (transferFromApi == null) {
+                    consoleService.printErrorMessage();
+                } else {
+                    System.out.println("TE bucks sent successfully!");
+                }
+            }
         }
 	}
 
 	private void requestBucks() {
 		// TODO Auto-generated method stub
-		
+        System.out.println("We're sorry, that service is currently unavailable.");
 	}
+
+    private void thankYou() {
+        System.out.println("Thank you for using TEnmo! Have a nice day!");
+    }
 
 }
